2c690c33151874066cfe5cbdaad6d261259abcfb
TheRokon <ozankonur@hotmail.com>
2017-01-02 19:09:58 +0200

    admin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -158,20 +158,20 @@ mod.controller('loginNormalCtrl', function($scope,$rootScope,$ionicHistory,share
 
           //Email
           firebase.auth().signInWithEmailAndPassword(cred.email,cred.password).then(function(result) {
-                $ionicHistory.nextViewOptions({
-                  historyRoot: true
-                });
-                $rootScope.extras = true;
-                sharedUtils.hideLoading();
-                $state.go('tabsController.shared', {}, {location: "replace"});
+            $ionicHistory.nextViewOptions({
+              historyRoot: true
+            });
+            $rootScope.extras = true;
+            sharedUtils.hideLoading();
+            $state.go('tabsController.shared', {}, {location: "replace"});
 
-              },
-              function(error) {
-                sharedUtils.hideLoading();
-                sharedUtils.showAlert("Authentication Error");
-                console.log("ERROR: "+error); 
-              }
-              );
+          },
+          function(error) {
+            sharedUtils.hideLoading();
+            sharedUtils.showAlert("Authentication Error");
+            console.log("ERROR: "+error); 
+          }
+          );
 
         }else{
           sharedUtils.showAlert("Entered data is not valid");
@@ -198,7 +198,7 @@ mod.controller('loginNormalCtrl', function($scope,$rootScope,$ionicHistory,share
 mod.controller('signupCtrl',function($scope,$rootScope,sharedUtils,$ionicSideMenuDelegate,$state,fireBaseData,$ionicHistory) {
     $rootScope.extras = false; // For hiding the side bar and nav icon
       $ionicSideMenuDelegate.canDragContent(false);  // To remove the sidemenu white space
-    $scope.signupEmail = function (formName, cred) {
+      $scope.signupEmail = function (formName, cred) {
 
       if (formName.$valid) {  // Check if the form data is valid or not
 
@@ -253,7 +253,7 @@ mod.controller('draftsCtrl', ['$scope', '$stateParams', // The following is the 
 function ($scope, $stateParams,$ionicSideMenuDelegate) {
         $ionicSideMenuDelegate.canDragContent(false);  // To remove the sidemenu white space
 
-}])
+      }])
 
 .controller('indexCtrl', function($scope,$rootScope,sharedUtils,$ionicHistory,$state,$q,$ionicSideMenuDelegate,$firebaseObject,fireBaseData) {
 
@@ -273,9 +273,9 @@ function ($scope, $stateParams,$ionicSideMenuDelegate) {
 
       }
     });
-$scope.setProfileImage=function(selection){
-function setOptions(srcType) {
-    var options = {
+    $scope.setProfileImage=function(selection){
+      function setOptions(srcType) {
+        var options = {
         // Some common settings are 20, 50, and 100
         quality: 50,
         destinationType: Camera.DestinationType.FILE_URI,
@@ -285,32 +285,32 @@ function setOptions(srcType) {
         mediaType: Camera.MediaType.PICTURE,
         allowEdit: true,
         correctOrientation: true  //Corrects Android orientation quirks
+      }
+      return options;
     }
-    return options;
-}
-function b64toBlob(b64Data, contentType, sliceSize) {
-  contentType = contentType || '';
-  sliceSize = sliceSize || 512;
+    function b64toBlob(b64Data, contentType, sliceSize) {
+      contentType = contentType || '';
+      sliceSize = sliceSize || 512;
 
-  var byteCharacters = atob(b64Data);
-  var byteArrays = [];
+      var byteCharacters = atob(b64Data);
+      var byteArrays = [];
 
-  for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    var slice = byteCharacters.slice(offset, offset + sliceSize);
+      for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+        var slice = byteCharacters.slice(offset, offset + sliceSize);
 
-    var byteNumbers = new Array(slice.length);
-    for (var i = 0; i < slice.length; i++) {
-      byteNumbers[i] = slice.charCodeAt(i);
+        var byteNumbers = new Array(slice.length);
+        for (var i = 0; i < slice.length; i++) {
+          byteNumbers[i] = slice.charCodeAt(i);
+        }
+
+        var byteArray = new Uint8Array(byteNumbers);
+
+        byteArrays.push(byteArray);
+      }
+
+      var blob = new Blob(byteArrays, {type: contentType});
+      return blob;
     }
-
-    var byteArray = new Uint8Array(byteNumbers);
-
-    byteArrays.push(byteArray);
-  }
-    
-  var blob = new Blob(byteArrays, {type: contentType});
-  return blob;
-}
     var srcType = Camera.PictureSourceType.SAVEDPHOTOALBUM;
     var options = setOptions(srcType);
 
@@ -319,23 +319,23 @@ function b64toBlob(b64Data, contentType, sliceSize) {
         // Camera.EncodingType (e.g., JPEG) must match the selected image type.
         options.targetHeight = 100;
         options.targetWidth = 100;
-    }
+      }
 
-    navigator.camera.getPicture(function cameraSuccess(imageData) {
+      navigator.camera.getPicture(function cameraSuccess(imageData) {
 
         // Do something with image
-		console.log('TRYING TO GET IMAGE');
-		saveToFirebase(imageData,'denemee.png');
-		console.log('BLOB IMAGE: '+BlobImage);
+        console.log('TRYING TO GET IMAGE');
+        saveToFirebase(imageData,'denemee.png');
+        console.log('BLOB IMAGE: '+BlobImage);
 
-    }, function cameraError(error) {
+      }, function cameraError(error) {
         console.debug("Unable to obtain picture: " + error, "app");
 
-    }, options);
-	
-	   function saveToFirebase(_imageBlob, _filename) {
+      }, options);
 
-      return $q(function (resolve, reject) {
+      function saveToFirebase(_imageBlob, _filename) {
+
+        return $q(function (resolve, reject) {
         // Create a root reference to the firebase storage
         var storageRef = firebase.storage().ref();
 
@@ -351,13 +351,13 @@ function b64toBlob(b64Data, contentType, sliceSize) {
         }, function () {
           // Handle successful uploads on complete
           var downloadURL = uploadTask.snapshot.downloadURL;
-			imageUrl=downloadURL;
+          imageUrl=downloadURL;
           // when done, pass back information on the saved image
           resolve(uploadTask.snapshot)
         });
       });
-}
-}
+      }
+    }
     $scope.logout=function(){
 
       sharedUtils.showLoading();
@@ -384,14 +384,16 @@ function b64toBlob(b64Data, contentType, sliceSize) {
 
   })
 
-mod.controller('createPostCtrl',function($scope,$rootScope,sharedUtils,$cordovaImagePicker, $cordovaFile,$ionicPlatform,$q,$ionicSideMenuDelegate,sharedpostService,$state,$firebaseObject,fireBaseData,$ionicHistory) {
+mod.controller('createPostCtrl',function($scope,$rootScope,sharedUtils,$cordovaImagePicker,$window,$cordovaFile,$ionicPlatform,$q,$ionicSideMenuDelegate,sharedpostService,$state,$firebaseObject,fireBaseData,$ionicHistory) {
     $rootScope.extras = false; // For hiding the side bar and nav icon
     var uid ;
-	var imageUrl;
+    var imageUrl=null;
+    var usertemp=null;
     //Check if user already logged in
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
         $scope.user_info=user; //Saves data to user_info
+        usertemp=user;
         $scope.user=  $firebaseObject(fireBaseData.refUser().child(user.uid));
         uid=user.uid;
       }else {
@@ -413,22 +415,37 @@ mod.controller('createPostCtrl',function($scope,$rootScope,sharedUtils,$cordovaI
 
         sharedUtils.showLoading();
 
-		
-            fireBaseData.refPost().push({    // set
-              postText: post.text,
-              postStatus: false,
-              postImage: imageUrl,
-              postVideo: "null",
-              postDirect:"1",
-			  postUserImg:""+user.image,
-              postUser: uid
-            }).then(function (success) {
-						sharedUtils.hideLoading();
-            sharedUtils.showAlert("Successfully sent watcher");
-			});
+        if(usertemp.image==null)
+          usertemp.image="null";
+        if(imageUrl==null)
+          imageUrl="null";
+        if(post)
+        {
+                    fireBaseData.refPost().push({    // set
+                      postText: post.text,
+                      postStatus: false,
+                      postImage: imageUrl,
+                      postVideo: "null",
+                      postDirect:"1",
+                      postUserImg:usertemp.image,
+                      postUser: uid
+                    }).then(function (success) {
+                      sharedUtils.hideLoading();
+
+                      sharedUtils.showAlert("Successfully sent watcher");
+                      $state.go('tabsController.shared', {}, {location: "replace"});
+
+                    });
+
+                  }
+                  else
+                  {
+                    sharedUtils.hideLoading();
+                    sharedUtils.showAlert("Please write something");
+                  }
         /*sharedpostService.add(post);
         */
-		
+
       }else{
         sharedUtils.hideLoading();
         sharedUtils.showAlert("Entered data is not valid");
@@ -446,12 +463,12 @@ mod.controller('createPostCtrl',function($scope,$rootScope,sharedUtils,$cordovaI
         saveToPhotoAlbum: true
 
       };
- var fileName, path;
+      var fileName, path;
 
       $cordovaImagePicker.getPictures(options)
-        .then(function (results) {
-          console.log('Image URI: ' + results[0]);
-          fileName = results[0].replace(/^.*[\\\/]/, '');
+      .then(function (results) {
+        console.log('Image URI: ' + results[0]);
+        fileName = results[0].replace(/^.*[\\\/]/, '');
           // modify the image path when on Android
           if ($ionicPlatform.is("android")) {
             path = cordova.file.cacheDirectory
@@ -460,24 +477,24 @@ mod.controller('createPostCtrl',function($scope,$rootScope,sharedUtils,$cordovaI
           }
           return $cordovaFile.readAsArrayBuffer(path, fileName);
         }, {
-        maximumImagesCount: 1
-    }).then(function (success) {
+          maximumImagesCount: 1
+        }).then(function (success) {
           // success - get blob data
           var imageBlob = new Blob([success], { type: "image/jpeg" });
 
           // missed some params... NOW it is a promise!!
           return saveToFirebase(imageBlob, fileName);
         }).then(function (_response) {
-			sharedUtils.hideLoading();
-            sharedUtils.showAlert("Image added");
-        }, function (error) {
+         sharedUtils.hideLoading();
+         sharedUtils.showAlert("Image added");
+       }, function (error) {
           // error
           console.log(error)
         });
-    }
-   function saveToFirebase(_imageBlob, _filename) {
+      }
+      function saveToFirebase(_imageBlob, _filename) {
 
-      return $q(function (resolve, reject) {
+        return $q(function (resolve, reject) {
         // Create a root reference to the firebase storage
         var storageRef = firebase.storage().ref();
 
@@ -498,18 +515,18 @@ mod.controller('createPostCtrl',function($scope,$rootScope,sharedUtils,$cordovaI
         }, function () {
           // Handle successful uploads on complete
           var downloadURL = uploadTask.snapshot.downloadURL;
-			imageUrl=downloadURL;
+          imageUrl=downloadURL;
           // when done, pass back information on the saved image
           resolve(uploadTask.snapshot)
         });
       });
-}
-  })
+      }
+    })
 
 
 
 mod.controller('adminCtrl', function ($scope,$rootScope,$ionicSideMenuDelegate,fireBaseData,$state,
-  $ionicHistory,$firebaseArray,$firebaseObject,sharedpostService,sharedUtils) {
+  $ionicHistory,$firebaseArray,$firebaseObject,sharedpostService,sharedUtils,$location) {
 
   var userid;
   var database = firebase.database();
@@ -528,7 +545,7 @@ mod.controller('adminCtrl', function ($scope,$rootScope,$ionicSideMenuDelegate,f
       });
       $rootScope.extras = false;
       sharedUtils.hideLoading();
-      $state.go('login', {}, {location: "replace"});
+      $window.location.href='#/login';
 
     }
   });
@@ -543,10 +560,6 @@ mod.controller('adminCtrl', function ($scope,$rootScope,$ionicSideMenuDelegate,f
       $ionicHistory.clearCache();
     }
   });
-
-  $scope.go=function(stateurl){
-    $state.go(stateurl, {}, {location: "replace"});
-  }
 
   $scope.loadAdminPost = function() {
     sharedUtils.showLoading();
@@ -563,8 +576,10 @@ mod.controller('adminCtrl', function ($scope,$rootScope,$ionicSideMenuDelegate,f
   $scope.approve=function(postItem){
     console.log(postItem);
     firebase.database().ref('posts/' + postItem).update({postStatus:true});
+    $location.url('/admin');
   }
   $scope.reject=function(postItem){
     firebase.database().ref('posts/' + postItem).update({postStatus:false});
+    $location.url('/admin');
   }
 })
